046975189a9d30155f1c6f599fd90cdd3b6484a5
daupaloffer <25135963+daupaloffer@users.noreply.github.com>
2021-12-09 23:53:16 +0000

    Setup basic main and commands scripts

---
+++ commands.py
@@ -0,0 +1,2 @@
+from nextcord.ext import commands
+
---
+++ main.py
@@ -0,0 +1,19 @@
+import nextcord
+import json
+import os
+
+from nextcord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = nextcord.Intents.default()
+dave = commands.Bot(command_prefix=",", intents=intents)
+
+@dave.event
+async def on_ready():
+    print('Dave Prime activated')
+    print(str(dave.user)+" | "+str(dave.user.id))
+
+
+dave.run(os.getenv("BOT_TOKEN"))
