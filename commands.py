0a7abccfb0f5689ad43671f2cb15bc38bc8a563d
daupaloffer <25135963+daupaloffer@users.noreply.github.com>
2021-12-10 00:10:33 +0000

    Setup commands file as nextcord extension with test command

--- commands.py
+++ commands.py
@@ -1,2 +1,11 @@
+import nextcord
 from nextcord.ext import commands
 
+
+@commands.command()
+async def testcommand(ctx):
+    await ctx.reply("idiot")
+
+
+def setup(dave):
+    dave.add_command(testcommand)
--- main.py
+++ main.py
@@ -10,10 +10,12 @@ load_dotenv()
 intents = nextcord.Intents.default()
 dave = commands.Bot(command_prefix=",", intents=intents)
 
+
 @dave.event
 async def on_ready():
     print('Dave Prime activated')
     print(str(dave.user)+" | "+str(dave.user.id))
 
 
+dave.load_extension("commands")
 dave.run(os.getenv("BOT_TOKEN"))
