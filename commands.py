fca757fef1f75b0c321438e53555f31bfa721620
daupaloffer <25135963+daupaloffer@users.noreply.github.com>
2022-02-16 08:27:15 +0000

    Port jsDiscordTTS owo command over using slash commands

--- commands.py
+++ commands.py
@@ -11,6 +11,34 @@ class slash_commands(commands.Cog):
     @nextcord.slash_command(guild_ids=guilds)
     async def testcommand(self, interaction):
         await interaction.response.send_message("idiot")
+    
+    @nextcord.slash_command(name="owo", description="owo-ify", guild_ids=guilds)
+    async def owoifier(self, 
+                       interaction, 
+                       message: str = nextcord.SlashOption(name="message", description="owo-ify provided message instead of previous message", required=False), 
+                       anonymous: bool = nextcord.SlashOption(name="anonymous", description="Make it look like it was Dave Prime who sent the message", required=False)):
+
+        if message: owo_payload = message
+        else:
+            owo_payload = (await interaction.channel.history(limit=2).flatten())[0]
+            if not owo_payload.content: # Maybe add owo_payload.author.bot too
+                await interaction.response.send_message("try again, idiot")
+                return
+            else:
+                owo_payload = owo_payload.content
+        
+        chars_to_replace = "lrLR"
+        for i in chars_to_replace:
+            if i.islower():
+                owo_payload = owo_payload.replace(i, "w")
+            else:
+                owo_payload = owo_payload.replace(i, "W")
+        
+        if anonymous:
+            await interaction.channel.send(owo_payload)
+            await interaction.response.send_message("Message sent!", ephemeral=True)
+        else:
+            await interaction.response.send_message(owo_payload)
 
 
 def setup(dave):
