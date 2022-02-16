6a387c7062eff914fc6220e006249e61376db10f
daupaloffer <25135963+daupaloffer@users.noreply.github.com>
2022-02-16 08:29:32 +0000

    Continue work on streamerboost
    Testing how to accurately judge if a member is starting or stopping streaming.
    Code to add and remove roles is already in place, just need to solidify what
     calls it

--- listeners.py
+++ listeners.py
@@ -13,9 +13,20 @@ async def listeny(ctx):
 
 @commands.Cog.listener()
 async def streamerboost(before, after):
+    # Sometimes they don't have an activity, so only do this if they do
     if after.activity:
-        if after.activity.type == nextcord.ActivityType.streaming:
+        if after.activity.type == nextcord.ActivityType.streaming and before.activity.type != nextcord.ActivityType.streaming:
             print(str(after.name)+" is streaming")
+            print(str(after.activity.details)+" "+str(after.activity.game))
+            if after.guild == 915057672515108935:
+                await after.add_roles([943381405801533471], reason="Member started streaming")
+    
+    if before.activity:
+        if before.activity.type == nextcord.ActivityType.streaming and after.activity.type != nextcord.ActivityType.streaming:
+            print(str(before.name)+" is no longer streaming")
+            print(str(after.activity.details)+" "+str(after.activity.game))
+            if before.guild == 915057672515108935:
+                await before.remove_roles([943381405801533471], reason="Member stopped streaming")
 
 
 def setup(dave):
