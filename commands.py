3b187853284c6b7fe40484cb5444a300c2b3366b
daupaloffer <25135963+daupaloffer@users.noreply.github.com>
2022-02-16 05:24:06 +0000

    Convert commands extension to fancy new slash commands, possible at last

--- commands.py
+++ commands.py
@@ -1,11 +1,17 @@
 import nextcord
 from nextcord.ext import commands
 
+guilds = [583429823125258303,915057672515108935]
 
-@commands.command()
-async def testcommand(ctx):
-    await ctx.reply("idiot")
+
+class slash_commands(commands.Cog):
+    def __init__(self, dave):
+        self.dave = dave
+    
+    @nextcord.slash_command(guild_ids=guilds)
+    async def testcommand(self, interaction):
+        await interaction.response.send_message("idiot")
 
 
 def setup(dave):
-    dave.add_command(testcommand)
+    dave.add_cog(slash_commands(dave))
