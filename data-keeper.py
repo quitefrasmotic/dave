4ff9c610556286737be4479bc107aaa2dc38b0d6
daupaloffer <25135963+daupaloffer@users.noreply.github.com>
2022-06-09 04:06:20 +0100

    Improve data storage system implementation
    Among other things:
    Clean up code
    Make sure data file exists before trying to get json data from it
    Create data file if it doesn't exist, fill with structure

--- data-keeper.py
+++ data-keeper.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import os.path
 
 from typing import Optional
 from typing import Literal
@@ -12,49 +13,77 @@ class DataKeeper(commands.Cog):
         self.bot = bot
 
     pref_cmd_description = "Change Dave Prime preferences"
+    pref_list = Literal["streamer_role"]
     pref_option_description = [
-        "This is the role the StreamerBoost feature will give to members that are streaming"
+        "This is the role the Streamerboost feature will give to members that are streaming",
+        "This is the channel that Dave Prime will send moderation alerts",
     ]
+    pref_cmd_group = app_commands.Group(
+        name="preferences", description="Set your Dave Prime preferences"
+    )
+    # preferences_group_show = app_commands.Group(name="show", description="Show your current Dave Prime preferences", parent=preferences_group)
 
-    @app_commands.command(name="preferences", description=pref_cmd_description)
-    @app_commands.describe(streamer_role=pref_option_description[0])
-    @app_commands.rename(streamer_role="streamerboost-role")
+    @pref_cmd_group.command(name="set", description=pref_cmd_description)
+    @app_commands.describe(
+        streamer_role=pref_option_description[0],
+        admin_channel=pref_option_description[1],
+    )
+    @app_commands.rename(
+        streamer_role="streamer-boost-role", admin_channel="admin-channel"
+    )
     async def save_prefs(
-        self, interaction: discord.Interaction, streamer_role: Optional[discord.Role]
+        self,
+        interaction: discord.Interaction,
+        streamer_role: Optional[discord.Role],
+        admin_channel: Optional[discord.TextChannel],
     ):
-        data = open("data.json", "r+")
-        in_array = None
-        json_data = json.load(data)
+        # Check if data file exists, if not create new one and generate basic json structure
+        if os.path.isfile("data.json"):
+            data = open("data.json", "r+")
+            json_data = json.load(data)
+        else:
+            data = open("data.json", "w+")
+            json_data = {"data": {"guilds": []}}
 
-        for i in json_data["data"]["guilds"]:
-            if i["guild"] == str(interaction.guild_id):
+        # Check if guild is already in the data, update value if so
+        in_array = None
+        for guild in json_data["data"]["guilds"]:
+            if guild["guild"] == str(interaction.guild_id):
+                if streamer_role:
+                    guild["streamer_role"] = str(streamer_role.id)
+
+                if admin_channel:
+                    guild["admin_channel"] = str(admin_channel.id)
+
                 in_array = True
 
+        # If guild isn't in data, add new entry into guild array with values
         if not in_array:
             json_data["data"]["guilds"].append(
                 {
                     "guild": str(interaction.guild_id),
-                    "streamer_role": str(streamer_role.id), # Linter complains about this - will fix underlying cause
+                    "streamer_role": str(streamer_role.id) if streamer_role else None,
+                    "admin_channel": str(admin_channel.id) if admin_channel else None,
                 }
             )
-        else:
-            for guild_element in json_data["data"]["guilds"]:
-                if guild_element["guild"] == str(interaction.guild_id):
-                    json_data["data"]["guilds"][guild_element]["streamer_role"] = str(
-                        streamer_role.id
-                    )
 
+        # Go to beginning of file and truncate to overwrite file to avoid opening file twice with different modes
         data.seek(0)
         data.write(json.dumps(json_data, indent=4, sort_keys=True))
         data.truncate()
         data.close()
 
-        await interaction.response.send_message("Preference updated!")
+        await interaction.response.send_message("Preferences updated!", ephemeral=True)
 
     pref_list = Literal["streamer_role"]
 
     async def get_prefs(self, guild: int, pref: pref_list):
-        data = open("data.json", "r")
+        try:
+            data = open("data.json", "r")
+        except FileNotFoundError:
+            print("Data file not found!")
+            return None
+
         json_data = json.load(data)
 
         result = None
